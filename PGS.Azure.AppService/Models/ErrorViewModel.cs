46ce3ba8ae58a00eaa27e56d38ddc67b2b48aeb3
Tomasz Kielar <tkielar@pgs-soft.com>
2018-02-14 11:29:30 +0100

    Test app

---
+++ PGS.Azure.AppService/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PGS.Azure.AppService.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
